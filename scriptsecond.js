05a241da625d2bddcc2fd02b5c2df1cb3734ff84
DevAnn23 <abanaszek2@gmail.com>
2018-04-21 11:02:06 +0200

    new files

--- scriptsecond.js
+++ scriptsecond.js
@@ -1,9 +1,22 @@
 const circle = document.querySelector(".circle"),
 	info = document.querySelector(".info"),
-	section = document.querySelector(".no-vue");
-
+	section = document.querySelector(".no-vue"),
+	circleH = circle.height,
+	circleW = circle.width,
+ fruits = document.getElementsByClassName('fruit');
 var positionValtop = circle.offsetTop;
 var positionValleft = circle.offsetLeft;
+
+var div = document.createElement("div");
+	div.style.top = positionValtop + circleH + "px";
+	div.style.left = positionValleft + circleW / 2 + "px";
+	div.style.borderRadius = "50%";
+	section.appendChild(div);
+	div.style.backgroundColor = "red";
+	div.style.height = 2 + "px";
+	div.style.width = 2 + "px";
+	div.style.position = "absolute";
+
 
 
 
@@ -11,16 +24,15 @@ var positionValleft = circle.offsetLeft;
 //		var y = e.clientY + 2;
 function addShadow() {
 	var div = document.createElement("div");
-div.style.top = positionValtop + 30 + "px";
-div.style.left = positionValleft + 10 + "px";
-div.style.borderRadius = "50%";
-section.appendChild(div);
-div.style.backgroundColor = "red";
-div.style.height = 5 + "px";
-div.style.width = 5 + "px";
-div.style.position = "absolute";
+	div.style.top = positionValtop + circleH + "px";
+	div.style.left = positionValleft + circleW / 2 + "px";
+	div.style.borderRadius = "50%";
+	section.appendChild(div);
+	div.style.backgroundColor = "red";
+	div.style.height = 2 + "px";
+	div.style.width = 2 + "px";
+	div.style.position = "absolute";
 
-console.log(circle.style.left);
 }
 //function winGame () {
 //	if(positionValtop)
@@ -33,23 +45,36 @@ console.log(circle.style.left);
 const move = (e) => {
 
 	if (e.keyCode === 39) {
-		positionValleft += 2;
+		positionValleft += 5;
 	}
 	if (e.keyCode === 37) {
-		positionValleft -= 2;
+		positionValleft -= 5;
 	}
 	if (e.keyCode === 38) {
-		positionValtop += -2;
+		positionValtop += -5;
 	}
 	if (e.keyCode === 40) {
-		positionValtop += 2;
+		positionValtop += 5;
 	}
 
 
-circle.style.top = positionValtop + "px";
-circle.style.left = positionValleft + "px";
+	circle.style.top = positionValtop + "px";
+	circle.style.left = positionValleft + "px";
 
 	addShadow();
-	console.log("wykonane strzałkowo" + e.keyCode);
+	for (var i=0; i < fruits.length; i++) {
+	fruits[0].style.top = 330 + "px";
+	fruits[0].style.left = 600 + "px";
+	fruits[1].style.top = 490 + "px";
+	fruits[1].style.left = 1000 + "px";
+	fruits[2].style.top = 730 + "px";
+	fruits[2].style.left = 830 + "px";
+	console.log(positionValleft);
+
+	if (positionValleft + circleW / 2 >= fruits[i].offsetLeft && positionValleft + circleW / 2 <= fruits[i].offsetLeft + fruits[i].width && positionValtop + circleH > fruits[i].offsetTop && positionValtop + circleH <= fruits[i].offsetTop + fruits[i].height){
+		fruits[i].style.display = "none";
+	}
+
+};
 }
 window.addEventListener("keydown", move);
